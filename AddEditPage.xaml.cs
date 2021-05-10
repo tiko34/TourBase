50db73c94a9ac631629bd68b08ff1a837e54114b
Andrei <Andrei@Server>
2021-05-10 17:14:58 +0300

    Mission 4 completed

--- AddEditPage.xaml.cs
+++ AddEditPage.xaml.cs
@@ -20,15 +20,57 @@ namespace TourBase
     /// </summary>
     public partial class AddEditPage : Page
     {
-        public AddEditPage()
+        private Hotel _currentHotel = new Hotel();
+        public AddEditPage( Hotel selectedHotel)
         {
             InitializeComponent();
+            if (selectedHotel != null)
+            {
+                _currentHotel = selectedHotel;
+            }
+            DataContext = _currentHotel;
             ComboCountries.ItemsSource = ToursBaseEntities.GetContext().Country.ToList();
         }
 
         private void BtnSave_Click(object sender, RoutedEventArgs e)
         {
+            StringBuilder errors = new StringBuilder();
+            if (String.IsNullOrWhiteSpace(_currentHotel.Name))
+            {
+                errors.AppendLine("Укажите название отеля");
+            }
+            if (_currentHotel.CountOfStars < 1 ||_currentHotel.CountOfStars >5)
+            {
+                errors.AppendLine("Кол-во звёзд - число от 1 до 5");
+            }
+            if (_currentHotel.Country == null)
+            {
+                errors.AppendLine("Выберите страну");
+            }
+            if (errors.Length > 0)
+            {
+                MessageBox.Show(errors.ToString());
+                return;
+            }
+            if (_currentHotel.id == 0)
+            {
+                ToursBaseEntities.GetContext().Hotel.Add(_currentHotel);
+            }
+            try
+            {
+                ToursBaseEntities.GetContext().SaveChanges();
+                MessageBox.Show("Информация сохранена!");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString()); 
+            }
+            Manager.MainFrame.GoBack();
+        }
 
+        private void ComboCountries_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            _currentHotel.Country = (Country)ComboCountries.SelectedItem;
         }
     }
 }
--- HotelsPage.xaml.cs
+++ HotelsPage.xaml.cs
@@ -26,24 +26,46 @@ namespace TourBase
             DGridHotels.ItemsSource = ToursBaseEntities.GetContext().Hotel.ToList();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            Manager.MainFrame.Navigate(new AddEditPage());
-        }
+       
 
         private void BtnEdit_Click(object sender, RoutedEventArgs e)
         {
-
+            Manager.MainFrame.Navigate(new AddEditPage((sender as Button).DataContext as Hotel));
         }
 
         private void BtnAdd_Click(object sender, RoutedEventArgs e)
         {
-            Manager.MainFrame.Navigate(new AddEditPage());
+             Manager.MainFrame.Navigate(new AddEditPage(null));
         }
 
         private void BtnDelete_Click(object sender, RoutedEventArgs e)
         {
+            var hotelsForRemoving = DGridHotels.SelectedItems.Cast<Hotel>().ToList();
+            if (MessageBox.Show($"Вы точно хотите удалить следуюшие {hotelsForRemoving.Count()}", "Внимание!",MessageBoxButton.YesNo,MessageBoxImage.Question)==MessageBoxResult.Yes);
+            {
+                try 
+                {
+                    ToursBaseEntities.GetContext().Hotel.RemoveRange(hotelsForRemoving);
+                    ToursBaseEntities.GetContext().SaveChanges();
+                    MessageBox.Show("Данные удалены");
+                    DGridHotels.ItemsSource = null;
+                    DGridHotels.ItemsSource = ToursBaseEntities.GetContext().Hotel.ToList();
+                }
+                catch   (Exception ex) 
+                {
+                    MessageBox.Show(ex.Message.ToString());
+                }
+            }
+        }
 
+        private void Page_IsVisibleChanged(object sender, DependencyPropertyChangedEventArgs e)
+        {
+            if (Visibility == Visibility.Visible)
+            {
+                ToursBaseEntities.GetContext().ChangeTracker.Entries().ToList().ForEach(p => p.Reload());
+                DGridHotels.ItemsSource = ToursBaseEntities.GetContext().Hotel.ToList();
+
+            }
         }
     }
 }
